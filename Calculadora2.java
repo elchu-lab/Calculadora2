cf398b4ae8af2434b9c04d2b93b9f3ead4fd9224
chu <sebasuazo30@gmail.com>
2025-02-22 19:59:20 -0300

    Agregando comentarios y optimizando case

--- Calculadora2.java
+++ Calculadora2.java
@@ -34,34 +34,27 @@ public class Calculadora2 {
         return calculadora.next().charAt(0);
     }
 
+    //funcion para detectar la cantidad de operdaores que quiera el usuario
     public static void entradaMatematicaUsuario(){
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("introduce la operacion: ");
-        String linea = entrada.nextLine(); 
-        String[] partes = linea.split(" "); //Separar por espacios
-        char operador = partes[1].charAt(0);
-        switch (operador) {
-            case '+':
-                System.out.println(suma(Integer.parseInt(partes[0]), Integer.parseInt(partes[2])));
-                break;
-            case '-':
-                System.out.println(resta(Integer.parseInt(partes[0]), Integer.parseInt(partes[2])));
-                break;
-            case '*':
-            if (partes.length == 3) {
-                System.out.println(multiplicacion(Double.parseDouble(partes[0]), Double.parseDouble(partes[2])));
-            } else {System.out.println(multiplicacion(Double.parseDouble(partes[0]), Double.parseDouble(partes[2]), Double.parseDouble(partes[4])));
-
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println("introduce la operacion: ");
+            String linea = entrada.nextLine();
+            String[] partes = linea.split(" "); //Funcion split para separar los elementos de la cadena el " " los separa creando una lista con indi
+            char operador = partes[1].charAt(0);
+            switch (operador) {
+                case '+' -> System.out.println(suma(Integer.parseInt(partes[0]), Integer.parseInt(partes[2]))); //Aqui el break se sustituye por "->" se aplica de forma implicita
+                case '-' -> System.out.println(resta(Integer.parseInt(partes[0]), Integer.parseInt(partes[2])));
+                case '*' -> {
+                    if (partes.length == 3) { //si la longitud de la cadena es 3 entonces solo se multiplican dos numeros
+                        System.out.println(multiplicacion(Double.parseDouble(partes[0]), Double.parseDouble(partes[2])));
+                    } else {System.out.println(multiplicacion(Double.parseDouble(partes[0]), Double.parseDouble(partes[2]), Double.parseDouble(partes[4])));
+                    
+                    }
+                }
+                case '/' -> System.out.println(division(Float.parseFloat(partes[0]), Float.parseFloat(partes[2])));
+                default -> throw new AssertionError();
             }
-            break;
-            case '/':    
-                System.out.println(division(Float.parseFloat(partes[0]), Float.parseFloat(partes[2])));
-                break;
-            default:
-                throw new AssertionError();
         }
-        
-        entrada.close();
         
     }
 
